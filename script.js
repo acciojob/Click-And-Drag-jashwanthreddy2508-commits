6bd1c80f8d1ee3c5a19f2a1433a7eb3554a9716c
jashwanthreddy2508-commits <jashwanthreddy2508@gmail.com>
2026-08-20 23:32:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-// Your code here.
+let grid = document.querySelectorAll(".item");
+let container = document.querySelector(".container");
+
+let selectedItem = null;
+
+grid.forEach((item) => {
+
+    item.addEventListener("mousedown", () => {
+        selectedItem = item;
+        item.style.position = "absolute";
+    });
+
+});
+
+document.addEventListener("mousemove", (event) => {
+
+    if (selectedItem) {
+
+        let rect = container.getBoundingClientRect();
+
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+
+        // Keep inside container
+        if (x < 0) x = 0;
+        if (y < 0) y = 0;
+
+        if (x > rect.width - 70) {
+            x = rect.width - 70;
+        }
+
+        if (y > rect.height - 70) {
+            y = rect.height - 70;
+        }
+
+        selectedItem.style.left = x + "px";
+        selectedItem.style.top = y + "px";
+    }
+
+});
+
+document.addEventListener("mouseup", () => {
+    selectedItem = null;
+});
+
